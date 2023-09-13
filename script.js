a446333f8047ed7dc6a6e9899c5e834fee1ab026
Tyler Hudson <tylerhudson96@gmail.com>
2023-09-13 16:02:42 -0400

    Implemented for loop to populate all books stored in library array

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const myLibrary = [];
+const parentContainer = document.getElementById("library-parent-container");
+
+const myLibrary = [
+  {
+    title: "Sunrise Paradise",
+    author: "Skinny Legged Man",
+    pages: "155",
+    status: "Unread",
+  },
+];
 
 function Book(title, author, pages, status) {
   this.title = title;
@@ -9,4 +18,17 @@ function Book(title, author, pages, status) {
 
 function addBookToLibrary(newBook) {
   myLibrary.push(newBook);
+}
+
+for (let i = 0; i < myLibrary.length; i++) {
+  let newEntryCard = document.createElement("div");
+  let newEntryContent = document.createElement("p");
+  newEntryContent.innerText = `
+  Title: ${myLibrary[i].title}
+  Author: ${myLibrary[i].author}
+  Length: ${myLibrary[i].pages} pages
+  Status: ${myLibrary[i].status}
+  `;
+  newEntryCard.appendChild(newEntryContent);
+  parentContainer.append(newEntryCard);
 }
